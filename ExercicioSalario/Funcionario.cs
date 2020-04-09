97d428752dfb7c3ace86620a416eb74840ebb381
Igor Gaspar <igor.elvis@hotmail.com>
2020-04-09 16:48:35 -0300

    Programa que calcula o salario de um funcionario descontando o imposto.

---
+++ ExercicioSalario/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ExercicioSalario {
+    class Funcionario {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido() {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem) {
+            SalarioBruto += (SalarioBruto * porcentagem / 100.0);
+        }
+
+        public override string ToString() {
+            return Nome + ", R$ " 
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
